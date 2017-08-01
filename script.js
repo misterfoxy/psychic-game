9f24b76ff8aadce9e4b4a3f5e97ab2e39ebe3fea
Fox <michaelscottfox1@gmail.com>
2017-08-01 15:26:20 -0700

    implemented various functions for psychic game

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
 'o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var guessedLetters=[];
 
 function chooseLetter(letters){
@@ -16,15 +16,33 @@ function updateTimer(){
   guessesLeft.innerHTML = number;
 }
 
+function resetTimer(){
+  var guessesLeft = document.getElementById('guessesLeft');
+  var number = guessesLeft.innerHTML;
+  number = 10;
+  guessesLeft.innerHTML = number;
+
+}
+
 function checkTimer(){
   var guessesLeft = document.getElementById('guessesLeft');
   var number = guessesLeft.innerHTML;
 
-  if(number < 1){
+  var numLosses = document.getElementById('losses');
+  var losses = numLosses.innerHTML;
 
+  if (number < 1){
+    losses ++;
+    document.getElementById('losses').innerHTML = losses;
+    resetGuessedLetters();
+    resetTimer();
   }
 }
 
+function resetGuessedLetters(){
+  guessedLetters=[];
+  document.getElementById('guessedLetters').innerHTML = guessedLetters;
+}
 
 
 document.onkeyup = function(event) {
@@ -34,12 +52,17 @@ document.onkeyup = function(event) {
   var answer = chooseLetter(letters);
   var guess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  for (var i = 0; i < guessedLetters.length; i++){
-    if (guess != guessedLetters[i]){
+    if (guess != answer){
       guessedLetters.push(" " + guess);
       document.getElementById('guessedLetters').innerHTML = guessedLetters;
     }
-  }
 
   checkTimer();
+
+  if(guess === answer){
+    wins++;
+    document.getElementById('wins').innerHTML = wins;
+    resetGuessedLetters();
+    resetTimer();
+  }
 };
